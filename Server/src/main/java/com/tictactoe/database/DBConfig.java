af4d9bed83d9fd57e378732b4c59e60666cfbdf6
onepiece <onepiece@192.168.1.12>
2020-01-07 04:55:47 +0200

    add DB config in gitignore

--- Server/src/main/java/com/tictactoe/database/DBConfig.java
+++
@@ -1,9 +0,0 @@
-package com.tictactoe.database;
-
-public interface DBConfig {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/tictactoe";
-    //    static final String DB_DRV = "com.mysql.jdbc.Driver";
-    static final String DB_DRV = "com.mysql.cj.jdbc.Driver";
-    static final String DB_USER = "root";
-    static final String DB_PASSWD = "Root123#";
-}
